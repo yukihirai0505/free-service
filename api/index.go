7c806f0bdb0ff6bded26a3b413aba652ba5003ae
Yuki Hirai <yukihirai0505@gmail.com>
2019-03-01 09:14:15 +0900

    Add conf package

--- api/index.go
+++ api/index.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"fmt"
+	"github.com/yukihirai0505/free-service/api/conf"
+	"log"
 	"net/http"
+	"os"
 )
 
+// mongodb://yabaiwebyasan:<PASSWORD>@cluster0-shard-00-00-lrlto.mongodb.net:27017,cluster0-shard-00-01-lrlto.mongodb.net:27017,cluster0-shard-00-02-lrlto.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true
 func Handler(w http.ResponseWriter, r *http.Request) {
+	conf.LoadEnv()
+	log.Println(os.Getenv("MONGO_USER"))
 	fmt.Fprintf(w, "I'm hoge")
 }
